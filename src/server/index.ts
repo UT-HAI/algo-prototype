0e23e041bf79491adbfd60de642d20bdee94df3f
owalker10 <36974651+owalker10@users.noreply.github.com>
2021-10-20 18:02:16 -0500

    changed express routing to work with react-router

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,7 @@
 
 import express, {Request, Response, Router, Express} from 'express';
 import router from './route';
+import path from 'path';
 import DBConnect from "./dbConfigs";
 import { RequestHandler } from 'express-serve-static-core';
 
@@ -18,19 +19,23 @@ const port: number = Number(process.env.PORT) || 8050; // set our port
 
 // connect to database. right now it's just working with mongodb
 // but in near future it will be configured for other databases as well
-DBConnect.dbConnection();
+// DBConnect.dbConnection();
 
 // Send index.html on root request
-app.use(express.static('dist'));
-app.get('/', (req:Request, res:Response) => {
-    console.log('sending index.html');
-    res.sendFile('/dist/index.html');
-});
+app.use(express.static(__dirname + '/../dist'));
 
 // REGISTER ROUTES
 // all of the routes will be prefixed with /api
 const routes: Router[] = Object.values(router);
 app.use('/api', routes);
+
+// using react-router so need to catch all other URLs and render the root HTML
+app.get('*', (req:Request, res:Response) => {
+    console.log('sending index.html');
+    res.sendFile(path.resolve(__dirname,'..', 'dist', 'index.html'))
+});
+
+
 
 // START THE SERVER
 // =============================================================================
