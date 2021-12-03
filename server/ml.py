c46503ef07dea214d9c8694a93d2f6c1e5d0d888
anqingchen <benjamin.c0427@gmail.com>
2021-12-02 18:45:48 -0600

    update data, removing bachelor's held and reworking ml endpoint

--- server/ml.py
+++ server/ml.py
@@ -9,27 +9,21 @@ target_col = 'Admission Decision'
 def select_features(df_train, df_test, features):
     # TODO: implement features filters, and check if one-hot is needed
     # df.drop(...)
-    all_features = [*features, 'Subject ID', target_col]
+    all_features = ['Subject ID', target_col]
+
+    for feature in features:
+        if feature == 'Ethnicity':
+            all_features.extend([
+                'American Indian or Alaska Native', 'Asian',
+                'Black or African American', 'Hispanic',
+                'Native Hawaiian or Other Pacific Islander', 'Not Reported',
+                'White'
+            ])
+        else:
+            all_features.append(feature)
 
     df_selected_train = df_train[all_features]
     df_selected_test = df_test[all_features]
-
-    # process gender
-    if 'Gender' in features:
-        df_selected_train.loc[:, 'Gender'] = df_selected_train['Gender'].apply(
-            lambda x: 1 if x == 'Male' else 0)
-        df_selected_test.loc[:, 'Gender'] = df_selected_test['Gender'].apply(
-            lambda x: 1 if x == 'Male' else 0)
-
-    # process ethnicity
-    if 'Ethnicity' in features:
-
-        def process_ethnicity(df):
-            one_hot = pd.get_dummies(df['Ethnicity'])
-            return df.drop('Ethnicity', axis=1).join(one_hot)
-
-        df_selected_test = process_ethnicity(df_selected_test)
-        df_selected_train = process_ethnicity(df_selected_train)
 
     X_test = df_selected_test.drop([target_col, 'Subject ID'], axis=1)
     y_test = df_selected_test[target_col].apply(lambda x: 0 if x == 'D' else 1)
@@ -40,6 +34,7 @@ def select_features(df_train, df_test, features):
 
 
 def train_and_predict(X_train, y_train, X_test):
+    print(X_test.shape)
     pipe = make_pipeline(StandardScaler(),
                          LogisticRegression(random_state=42, max_iter=200))
     pipe.fit(X_train, y_train)
